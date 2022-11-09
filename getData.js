19ae694bf2475b59a7eb2e5dd9b5707ac8f8224f
Daniel-k-dev <juyeon10120@naver.com>
2022-11-09 23:28:04 +0900

    feat : Statusbar works well
    - Issue
    
        - Double checking id must be work by Ajax
          it work well but differnt we thought.

---
+++ getData.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+module.exports = {
+  /**
+   * Form 에서 데이터를 받아 객체를 리턴하는 메서드
+   * @param {*} request
+   * @param {*} response
+   * @returns 객체타입의 값을 리턴
+   */
+  getFormData(request, response) {
+    return request.body;
+  },
+};
--- validation.js
+++ validation.js
@@ -1,44 +1,27 @@
 const access = require("./DB/access");
-var express = require('express')
-var bodyParser = require('body-parser')
-
-
-var app = express()
-
-app.use(bodyParser.urlencoded({ extended: false }))
+const { alarm } = require("./template");
 
 module.exports = {
-  /**
-   * Form 에서 데이터를 받아 객체를 리턴하는 메서드
-   * @param {*} request 
-   * @param {*} response 
-   * @returns 객체타입의 값을 리턴 
-   */
-  getFormData (request, response) {     
-    return request.body;
-  },
-
   /**
    * 로그인 검증 로직
    * @param {*} request 
    * @param {*} response 
-   * @param {*} userInput : 유저정보에 대한 객체를 매개변수로 추가 getFormData() 사용
-   * @returns 
+   * @param {*} userInput : 유저정보에 대한 객체를 매개변수로 추가 getFormData() 사용 
    */
    verifyLogin  (request, response, userInput) {
-    const object = access.userData(request, response);
-    const user_data = object.user_data;
-    const id_list = object.id_list;
-    const rows = object.count_user_datas_row;
+    const userData = access.userData(request, response);
+    const userDataTable = userData.userDataTable;
+    const idList = userData.idList;
+    const rows = userData.userDataTableRowsCount;
     const ID = userInput.ID;
     const password = userInput.PW;
 
     for (let i = 0; i < rows; i++) {
-      if (ID === id_list[i] & password == user_data[i].user_password) {
+      if (ID === idList[i] & password == userDataTable[i].user_password) {
         request.session.is_logined = true;
         request.session.userid = ID;
         response.redirect("/live");
-        return; 
+        break; 
       } else if (rows - 1 == i) {
         console.log("아이디가 혹은 패스워드가 잘못 되었습니다.");
         response.redirect('back');
@@ -55,17 +38,16 @@ module.exports = {
    * @returns 
    */
    verifySignup  (request, response, userInput) {
-     const object = access.userData(request, response);
-     const user_data = object.user_data;
-     const id_list = object.id_list;
-     const rows = object.count_user_datas_row;
+     const userData = access.userData(request, response);
+     const idList = userData.idList;
+     const rows = userData.userDataTableRowsCount;
      const ID = userInput.ID;
      const password = userInput.pwd;
      const contrastPassword = userInput.contrastPwd;
 
      if (password === contrastPassword) {
        for (let i = 0; i < rows; i++) {
-         if (ID === id_list[i]) {
+         if (ID === idList[i]) {
            console.log("아이디 중복");
            response.redirect("back");
            break;
@@ -82,25 +64,31 @@ module.exports = {
      }
    },
 
-  /**
-   * 
-   * @param {*} trueCode 
-   * @param {*} falseCode 
-   */
-  checkLogin : (request, trueCode, falseCode) => {
-    console.log(request.session);
-    if (request.session.is_logined === true) {
-      console.log("true");
-      trueCode
-    } else {
-      falseCode
+
+  isOverTime : (alarmTime) => {
+    let browserHours = new Date().getHours(); 
+    let browserMinutes = new Date().getMinutes(); 
+
+    console.log("isoverTime")
+    let alarmTimes = alarmTime.departure_time
+    console.log(alarmTimes)
+    for (let col = 0 ;col < alarmTimes.length; col++) {
+      if (alarmTimes.substring(col,col+1) === ":") {
+        let departureHour = parseInt(alarmTimes.substring(0,col)); 
+        let departureMin = parseInt(alarmTimes.substring(col+1,parseInt(alarmTimes.length + 1)))    
+        if (browserHours < departureHour) {
+          return false;
+        } else if (browserHours === departureHour && browserMinutes <= departureMin) {
+          return false;
+        } else {
+          return true;
+        }
+      }
     }
+    
+
+
   },
 
-  // login () {
-  //   <script>
-      
-  //   </script>
-  //   return;
-  // }
 }
+
